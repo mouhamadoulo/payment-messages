baa59b405d15c3ca8d00ceb211234b12e9df3357
Mouhamadou Moustapha LO <mouhamadoulo39@gmail.com>
2026-07-20 21:04:35 +0200

    Init backend SpringBoot

---
+++ backend/src/main/java/com/bank/paymentmessages/PaymentMessagesApplication.java
@@ -0,0 +1,13 @@
+package com.bank.paymentmessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMessagesApplication.class, args);
+	}
+
+}
