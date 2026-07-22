9eebce6dfee50bfe28263fe04d6b1620fae3bc12
Mouhamadou Moustapha LO <mouhamadoulo39@gmail.com>
2026-07-23 00:07:52 +0200

    Modifs docker-compose + Listener + Pom

--- backend/src/main/java/com/bank/paymentmessages/mq/PaymentMessageListener.java
+++ backend/src/main/java/com/bank/paymentmessages/mq/PaymentMessageListener.java
@@ -2,6 +2,8 @@ package com.bank.paymentmessages.mq;
 
 
 import com.bank.paymentmessages.service.PaymentMessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +11,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class PaymentMessageListener {
 
+    private static final Logger log = LoggerFactory.getLogger(PaymentMessageListener.class);
+
     private final PaymentMessageService service;
 
     public PaymentMessageListener(PaymentMessageService service){
         this.service = service;
     }
 
-    @JmsListener(destination = "PAYMENT.IN")
+    @JmsListener(destination = "${ibm.mq.queue}", concurrency = "5-10")
     public void receive(String message){
+        log.info("Message IBM MQ reçu : {}", message);
+
         service.saveMessage(message);
     }
 
