5f2dfad5a1c2ede96c2b8103f9d1ed4b936da6bb
Mouhamadou Moustapha LO <mouhamadoulo39@gmail.com>
2026-07-23 21:22:34 +0200

    Correctifs Tests

--- backend/src/test/java/com/bank/paymentmessages/repository/PaymentMessageRepositoryTest.java
+++ backend/src/test/java/com/bank/paymentmessages/repository/PaymentMessageRepositoryTest.java
@@ -4,7 +4,7 @@ import com.bank.paymentmessages.entity.PaymentMessage;
 import com.bank.paymentmessages.entity.PaymentMessageStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
